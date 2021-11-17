2dc8fc8150fbe5cfaee0c77143b4b86e95000139
Marco <marcosmourabusiness@gmail.com>
2021-11-17 12:19:41 -0300

    update and add new challenges

---
+++ challenges/3.1.Odd_or_Even/main.py
@@ -0,0 +1,10 @@
+#Don't change the code below
+number = int(input("Which number do you want to check? "))
+#Don't change the code above
+
+#Write your code below this line
+
+if number % 2 == 0:
+  print(f"This is an even number.")
+else:
+  print("This is an odd number.")
