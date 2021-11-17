1b80edbcd60cdc66bbd5bbf8b03c52e7f138114f
Marco <marcosmourabusiness@gmail.com>
2021-11-17 12:23:14 -0300

    update and add new challenges

--- challenges/3.5.Love_Calculator/main.py
+++ challenges/3.5.Love_Calculator/main.py
@@ -12,15 +12,15 @@ t = lower_names.count("t")
 r = lower_names.count("r")
 u = lower_names.count("u")
 e = lower_names.count("e")
-first_digit = t + r + u + e
+true = t + r + u + e
 
 l = lower_names.count("l")
 o = lower_names.count("o")
 v = lower_names.count("v")
 e = lower_names.count("e")
-second_digit = l + o + v + e
+love = l + o + v + e
 
-score = int(str(first_digit) + str(second_digit))
+score = int(str(true) + str(love))
 
 if (score < 10) or (score > 90):
   print(f"Your score is {score}, you go together like coke and mentos.")
